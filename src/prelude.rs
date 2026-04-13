7500600936582c1683cee497be503c3b37f94bdc
nakulsoneji <nakulsoneji@berkeley.edu>
2026-04-13 01:44:40 -0700

    updated prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,6 @@
 pub use crate::{vector, matrix};
 pub use crate::linalg::{Matrix, Vector};
-pub use crate::linalg::matrix::*;
+pub use crate::linalg::matrix::Axis;
 pub use crate::ml::LinearModel;
 pub use crate::linalg::common::*;
 pub use crate::linalg::errors::*;
